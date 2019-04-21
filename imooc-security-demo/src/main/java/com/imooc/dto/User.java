7ccc7eb5a4056a8fbaa41f63794bb8123d41d2e8
zwyu123 <zwyu2017@163.com>
2019-04-21 22:00:17 +0800

    restful api

---
+++ imooc-security-demo/src/main/java/com/imooc/dto/User.java
@@ -0,0 +1,27 @@
+package com.imooc.dto;
+
+public class User {
+
+	
+	private String username;
+	
+	private String passwordString;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPasswordString() {
+		return passwordString;
+	}
+
+	public void setPasswordString(String passwordString) {
+		this.passwordString = passwordString;
+	}
+	
+	
+}
---
+++ imooc-security-demo/src/main/java/com/imooc/dto/UserQueryCondition.java
@@ -0,0 +1,5 @@
+package com.imooc.dto;
+
+public class UserQueryCondition {
+
+}
